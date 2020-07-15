066b6b6eb3a9fb8b592cca154ceec87d5e522a7c
Thiago Antunes Milhomens <68217811+ThiagoMilhomens@users.noreply.github.com>
2020-07-15 14:18:19 -0300

    Create nullUndefined.js

---
+++ exercicios-js/fudamentos/nullUndefined.js
@@ -0,0 +1,40 @@
+ /*let valor // nao inicializada
+
+console.log(valor)
+// undefined
+
+ //console.log(valor2) // valor2 sequer foi declarado
+// Erro - valor2 is not defined 
+
+valor = null // ausencia de valor nao aponta para nenhum objeto na memória
+
+console.log(valor)
+// null
+
+//----------------------------------//------------------------------------// */
+
+const produto = {}
+console.log(produto.preco)
+//undefined
+console.log(produto)
+//{}
+produto.preco = 3.50
+
+console.log(produto)
+//{ preco: 3.5 }
+
+produto.preco = undefined //Evite atribuir undefined! melhor seria null ou 0 neste caso de valor numérico
+console.log(!!produto.preco) //conversao para boolean
+//false
+console.log(produto)
+//{ preco: undefined }
+// Ou seja nao faz com que o objeto deixe de ter um atributo...Melhor seria como a seguir.
+delete produto.preco
+console.log(produto) // devolverá um objeto vazio
+// {}
+
+produto.preco = null // sem preço 
+console.log(!!produto.preco)
+//false
+console.log(produto) 
+//{ preco: null }

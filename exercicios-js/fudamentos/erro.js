3a241e17101782289cbc1a05a21352c71a58a621
Thiago Antunes Milhomens <68217811+ThiagoMilhomens@users.noreply.github.com>
2020-07-21 18:28:11 -0300

    jul21

--- exercicios-js/fudamentos/erro.js
+++ exercicios-js/fudamentos/erro.js
@@ -1,7 +1,7 @@
-/*/ Tratamento de erro em Javascript
+// Tratamento de erro em Javascript
 
 function imprimirNomeGritado(obj) {
-    console.log(obj.name.toUppercase() + '!!!')
+    console.log(obj.name.toUpperCase() + '!!!')
 }
 
 const obj = { nome: 'Roberto' } // nome é um erro proposital para a notação ponto ".name"
@@ -9,7 +9,7 @@ imprimirNomeGritado(obj)
 //TypeError: Cannot read property 'toUppercase' of undefined
 
 // como fazer o tratamento para o caso acima.... 
-*/
+
 function tratarErroElancar(erro) {
     //throw new Error('...')
     throw 10
@@ -25,7 +25,7 @@ function tratarErroElancar(erro) {
 
 function imprimirNomeGritado(obj) {
     try {
-    console.log(obj.name.toUppercase() + '!!!')
+    console.log(obj.name.toUpperCase() + '!!!')
     } catch (e) {
         tratarErroElancar(e)
     } finally {
@@ -33,5 +33,13 @@ function imprimirNomeGritado(obj) {
     }
 }
 
-const obj = { name: 'Roberto' } 
+const obj = { nome: 'Roberto' } // Se eu concertar o erro e escrever "name:" o retorono é // Roberto!!! //final
 imprimirNomeGritado(obj)
+//final
+/*
+c:\Users\thiag\Documents\GitHub\WebDev-Studies\exercicios-js\fudamentos\tempCodeRunnerFile.js:3
+    throw 10
+    ^
+10
+(Use `node --trace-uncaught ...` to show where the exception was thrown)
+*/

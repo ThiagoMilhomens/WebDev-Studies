d3532a83a0fafbadd0aff14f0463e3f4cc57372b
Thiago Antunes Milhomens <68217811+ThiagoMilhomens@users.noreply.github.com>
2020-07-27 21:05:49 -0300

    jul27

---
+++ exercicios-js/função/thisEbind1.js
@@ -0,0 +1,19 @@
+//----------------------This e Bind---------------------//
+
+const pessoa = {
+    saudacao: 'Bom dia!',
+    falar() {
+        console.log(this.saudacao) // se não usar 'this' ele não retorna 'saudacao'
+    }
+}
+
+pessoa.falar()
+// Bom dia!
+
+const falar = pessoa.falar
+falar() // conflito entre paradigmas: funcional e OO
+// undefined
+
+const falar = pessoa.falar.bind(pessoa) // '.bind' ligou com a const 'pessoa' que contem 'saudacao'
+falar()
+// Bom dia!
---
+++ exercicios-js/função/thisEbind2.js
@@ -0,0 +1,39 @@
+
+
+function Pessoa() {
+    this.idade = 0
+
+    setInterval(function() {
+        this.idade++
+        console.log(this.idade)
+    }.bind(this), 1000) // '1000' define o tempo em milisegundos que a função 'setInterval' é disparada
+}
+
+new Pessoa 
+/*
+1
+2
+3
+4
+*/
+// Atalho do run code (ctrl + alt + N) e para encerrar (ctrl + alt + M)  
+
+//--------sem usar .bind------//
+
+function Pessoa() {
+    this.idade = 0
+    
+    const self = this  // outra forma de garantir que o valor do 'this' seja executado
+    setInterval(function() {
+        self.idade++
+        console.log(self.idade)
+    }/* .bind(this) */, 1000) 
+}
+
+new Pessoa 
+/*
+1
+2
+3
+4
+*/

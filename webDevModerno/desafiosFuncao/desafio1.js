938cdcf9ecaee5110851b20aebe9c2de7ca6a6c4
Thiago Antunes Milhomens <68217811+ThiagoMilhomens@users.noreply.github.com>
2020-07-29 22:07:18 -0300

    Update desafio1.js

--- webDevModerno/desafiosFuncao/desafio1.js
+++ webDevModerno/desafiosFuncao/desafio1.js
@@ -9,3 +9,11 @@ function valores(a, b) {
 }
 
 console.log(valores(3, 2))
+//{ soma: 5, sub: 1, mult: 6, div: 1.5 }
+
+//resposta
+function calcularOperacoes (operador1, operador2) {
+    console.log(operador1+operador2, operador1-operador2, operador1*operador2, operador1/operador2);
+}
+
+calcularOperacoes(1, 2)

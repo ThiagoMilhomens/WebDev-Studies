3baa09d38c90e3159fa6bffe9b5359e3366f97ee
Thiago Antunes Milhomens <68217811+ThiagoMilhomens@users.noreply.github.com>
2020-07-30 21:46:43 -0300

    Create objetoRevisão.js

---
+++ webDevModerno/objeto/objetoRevisão.js
@@ -0,0 +1,80 @@
+//--------------Revisão sobre Objeto-------------//
+//Objeto é uma coleção dinâmica (pode adicionar e remover) de pares chave/valor 
+
+const carro = new Object //O operador'new' istancia um objeto através da função 'Object'
+// Forma simples de criar objeto: const carro  = {}
+
+//adicionando atributos
+carro.nome = 'Palio'    //usando notação ponto
+carro['Marca'] = 'Fiat' //usando colchetes...
+carro.preco = 22000     
+
+console.log(carro)
+//{ nome: 'Palio', Marca: 'Fiat', preco: 22000 }
+
+//removendo atributos
+delete carro.preco
+delete carro['Marca']
+console.log(carro) 
+//{ nome: 'Palio' }
+
+//Exemplo de abstração utilizada em suposta corretora de seguros..
+
+const moto = {
+    modelo: 'CG 125',
+    valor: 5000,
+    proprietario: {
+        nome: 'Raul',
+        sobrenome: 'Silva',
+        endereco: {
+            logradouro: 'Rua Presidente Vargas',
+            numero: 123 
+        }
+    },
+    condutores: [{
+        nome: 'Junior',
+        idade: 23
+    },{
+        nome: 'Ana',
+        idade: 25
+    }],
+    calcularValorSeguro: function() { // funçâo dentro do objeto
+        // ....
+    }
+}
+
+/*
+calcularValorSeguro: function() {
+        // ....
+    }
+*/
+
+moto.proprietario.endereco.numero = 1000
+moto['proprietario'] ['endereco']['logradouro'] = 'Av Barão de Mauá'
+console.log(moto)
+/*
+{
+  modelo: 'CG 125',
+  valor: 5000,
+  proprietario: {
+    nome: 'Raul',
+    sobrenome: 'Silva',
+    endereco: { logradouro: 'Av Barão de Mauá', numero: 1000 }
+  },
+  condutores: [ { nome: 'Junior', idade: 23 }, { nome: 'Ana', idade: 25 } ],
+  calcularValorSeguro: [Function: calcularValorSeguro]
+}
+*/
+
+delete moto.condutores
+delete moto.proprietario.endereco
+delete moto.calcularValorSeguro 
+
+console.log(moto)
+/*
+{
+  modelo: 'CG 125',
+  valor: 5000,
+  proprietario: { nome: 'Raul', sobrenome: 'Silva' }
+}
+*/

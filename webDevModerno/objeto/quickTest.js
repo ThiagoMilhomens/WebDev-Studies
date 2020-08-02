df4f8c3fc7c82bdde60080f8564ed7677cff6e9d
Thiago Antunes Milhomens <68217811+ThiagoMilhomens@users.noreply.github.com>
2020-08-02 17:49:41 -0300

    02ago

---
+++ webDevModerno/objeto/quickTest.js
@@ -0,0 +1,18 @@
+// gerador de 6 dezenas para a mega sena
+
+// gerar numero aleatório entre 1 e 60
+// armazenar o numero em um array
+// repetir 
+
+
+
+
+
+
+
+//let dezenas = []
+
+let dezena = {
+    ((Math.random() * 60) + 1)
+    }
+ console.log(dezena)

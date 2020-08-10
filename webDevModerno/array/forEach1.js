a578bc53e963e81d424beeac99ed6fba51a62ea6
Thiago Antunes Milhomens <68217811+ThiagoMilhomens@users.noreply.github.com>
2020-08-10 08:54:23 -0300

    ago10

--- webDevModerno/array/forEach1.js
+++ webDevModerno/array/forEach1.js
@@ -1,3 +1,27 @@
  //-------------Foreach #01---------------//
-
  
+ //'.forEach()' é uma das formas de percorrer o array
+ //Assim eu posso determinar uma função callback(invoked), que será executada em cada um dos elementos do array 
+ //A função callback tem 3 parâmetros(value, index e array)
+
+
+const aprovados = [ 'Agatha', 'Aldo', 'Daniel', 'Raquel']
+
+aprovados.forEach(function(nome,indice){
+    console.log(`${indice + 1}) ${nome}`) // 
+})
+/*
+1) Agatha
+2) Aldo
+3) Daniel
+4) Raquel
+*/
+
+//usando arrow function e chamando apenas o primeiro parâmentro do callback
+aprovados.forEach(nome => console.log(nome))
+/*
+Agatha
+Aldo
+Daniel
+Raquel
+*/

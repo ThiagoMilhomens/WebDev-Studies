2b0bf32359cfa8434035f45a1f28310fa4a01a79
Thiago Antunes Milhomens <68217811+ThiagoMilhomens@users.noreply.github.com>
2020-08-10 09:27:13 -0300

    Update forEach1.js

--- webDevModerno/array/forEach1.js
+++ webDevModerno/array/forEach1.js
@@ -1,6 +1,7 @@
  //-------------Foreach #01---------------//
  
  //'.forEach()' é uma das formas de percorrer o array
+ //É uma função em loop que fechará o laço no último elemento do array
  //Assim eu posso determinar uma função callback(invoked), que será executada em cada um dos elementos do array 
  //A função callback tem 3 parâmetros(value, index e array)
 
@@ -8,7 +9,7 @@
 const aprovados = [ 'Agatha', 'Aldo', 'Daniel', 'Raquel']
 
 aprovados.forEach(function(nome,indice){
-    console.log(`${indice + 1}) ${nome}`) // 
+    console.log(`${indice + 1}) ${nome}`)
 })
 /*
 1) Agatha
@@ -17,8 +18,29 @@ aprovados.forEach(function(nome,indice){
 4) Raquel
 */
 
-//usando arrow function e chamando apenas o primeiro parâmentro do callback
+//Chamando o terceiro parâmetro array, por ser loop ele vai retornar pelo quantidade do index  
+aprovados.forEach(function(nome,indice,array){
+    console.log(array)
+})
+/*
+[ 'Agatha', 'Aldo', 'Daniel', 'Raquel' ]
+[ 'Agatha', 'Aldo', 'Daniel', 'Raquel' ]
+[ 'Agatha', 'Aldo', 'Daniel', 'Raquel' ]
+[ 'Agatha', 'Aldo', 'Daniel', 'Raquel' ]
+*/
+
+//usando arrow function e chamando apenas o primeiro parâmetro do callback
 aprovados.forEach(nome => console.log(nome))
+/*
+Agatha
+Aldo
+Daniel
+Raquel
+*/
+
+// armazenando a função em uma variável
+const exibirAprovados = nomesAprovados => console.log(nomesAprovados)
+aprovados.forEach(exibirAprovados)
 /*
 Agatha
 Aldo

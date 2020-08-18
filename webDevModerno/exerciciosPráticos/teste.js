990293a430a16a3feb6e8ecac883fc9c03927af3
Thiago Antunes Milhomens <68217811+ThiagoMilhomens@users.noreply.github.com>
2020-08-17 21:05:52 -0300

    Update teste.js

--- webDevModerno/exerciciosPráticos/teste.js
+++ webDevModerno/exerciciosPráticos/teste.js
@@ -1,3 +1,19 @@
+//let dezena = getInteiroAleatorio(1, 61)
+
+function getInteiroAleatorio(min, max) {
+    const valor = Math.random() * (max - min) + min
+    return Math.floor(valor)
+}
+let opcao = 1
+
+while (opcao != 60) {      
+    opcao = getInteiroAleatorio(1, 61)    
+    console.log(`Opção escolhida foi ${opcao}.`)    
+}
+
+console.log('Até a próxima!') 
+
+/*
 function teste(num) {
     if (num > 7)
     console .log(num)
@@ -7,3 +23,4 @@ function teste(num) {
 
 //teste(6)
 teste(8)
+*/

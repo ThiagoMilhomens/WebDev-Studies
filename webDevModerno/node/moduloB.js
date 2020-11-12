e7c32099bf9c7a3fbc0094774ad3ba92b49eab8b
Thiago Antunes Milhomens <68217811+ThiagoMilhomens@users.noreply.github.com>
2020-11-12 20:38:56 -0300

    nov11

---
+++ webDevModerno/node/moduloB.js
@@ -0,0 +1,7 @@
+module.exports = {
+    bomdia: 'Bom dia',
+    boaNoite() {
+        return 'Boa Noite'
+    }
+}
+
---
+++ webDevModerno/node/moduloCliente.js
@@ -0,0 +1,9 @@
+const moduloA = require('./moduloA.js')
+const moduloB = require('./moduloB.js')
+
+ console.log(moduloA.ola)
+ console.log(moduloA.bemVindo)
+ console.log(moduloA.ateLogo)
+
+ console.log(moduloB.bomdia)
+ console.log(moduloB.boaNoite())

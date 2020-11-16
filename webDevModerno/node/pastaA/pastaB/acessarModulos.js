e12a1564bb089394ce37bfbf735b7bb96d1c8c2e
Thiago Antunes Milhomens <68217811+ThiagoMilhomens@users.noreply.github.com>
2020-11-16 20:23:42 -0300

    nov16

---
+++ webDevModerno/node/pastaA/pastaB/acessarModulos.js
@@ -0,0 +1,11 @@
+const moduloA = require('../../moduloA') // respeitar Maiúsculo pq linux é case sensitive
+
+console.log(moduloA.ola)
+
+console.log(moduloA.bemVindo)
+
+console.log(moduloA.ateLogo)
+
+const saudacao = require('saudacao') // exemplo importando do index.js dentro do node_modules
+
+console.log(saudacao.ola)
